9c7513e774d31b799440b2d8be3fccd6aae1df61
vTxh <tang_txh@icloud.com>
2021-04-02 11:34:02 +0800

    test

---
+++ simpleBookSell/simpleBookSell/Controllers/bookController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simpleBookSell.Controllers
+{
+    public class bookController : Controller
+    {
+        // GET: book
+        // 首页展示图书列表
+        public ActionResult bookList()
+        {
+            List<db.Books> list = db.bll.books.getBooks();
+
+            return View(list);
+        }
+
+        //图书新增界面展示
+        [HttpGet]
+        public ActionResult insert()
+        {
+            db.Books entry = new db.Books();
+            return View(entry);
+        }
+
+        //保存新增图书数据
+        [HttpPost]
+        public ActionResult insert(string AuthorName, string Title, Nullable<decimal> Price)
+        {
+            db.bll.books.insert(AuthorName, Title, Price);
+            return RedirectToAction("bookList");
+        }
+
+        //图书新增界面展示-方法2
+        [HttpGet]
+        public ActionResult insertEntry()
+        {
+            db.Books entry = new db.Books();
+            return View(entry);
+        }
+
+        //保存新增图书数据-方法2
+        [HttpPost]
+        public ActionResult insertEntry(db.Books entry)
+        {
+            db.bll.books.insert(entry);
+            return RedirectToAction("bookList");
+        }
+
+        //图书修改界面展示
+        [HttpGet]
+        public ActionResult update(int id)
+        {
+            db.Books entry = db.bll.books.getEntry(id);
+            return View(entry);
+        }
+
+        //保存修改图书数据
+        [HttpPost]
+        public ActionResult update(int BookId, string AuthorName, string Title, Nullable<decimal> Price)
+        {
+            db.bll.books.update(BookId, AuthorName, Title, Price);
+            return RedirectToAction("bookList");
+        }
+
+
+        //图书修改界面展示-方法2
+        [HttpGet]
+        public ActionResult updateEntry(int id)
+        {
+            db.Books entry = db.bll.books.getEntry(id);
+            return View(entry);
+        }
+        //保存修改图书数据-方法2
+        [HttpPost]
+        public ActionResult updateEntry(db.Books entry)
+        {
+            db.bll.books.update(entry);
+            return RedirectToAction("bookList");
+        }
+
+        //图书修改界面展示-方法3
+        [HttpGet]
+        public ActionResult update3(int id)
+        {
+            db.Books entry = db.bll.books.getEntry(id);
+            return View(entry);
+        }
+        //保存修改图书数据-方法3
+        [HttpPost]
+        public ActionResult update3(db.Books entry)
+        {
+            db.bll.books.update3(entry);
+            return RedirectToAction("bookList");
+        }
+
+        //图书删除
+        public ActionResult delete(int id)
+        {
+            db.bll.books.Delete(id);
+            return RedirectToAction("bookList");
+        }
+
+    }
+}
---
+++ simpleBookSell/simpleBookSell/Controllers/storeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace simpleBookSell.Controllers
+{
+    public class storeController : Controller
+    {
+        // 首页展示图书列表
+        public ActionResult bookList()
+        {
+            List<db.Books> list = db.bll.books.getBooks();
+
+            return View(list);
+        }
+
+        // 购书
+        [HttpGet]
+        public ActionResult order(int id)
+        {
+            db.Books entry = db.bll.books.getEntry(id);
+
+            return View(entry);
+        }
+
+        // 下单确认购买
+        [HttpPost]
+        public ActionResult order(int bookId, int num, string address)
+        {
+            db.bll.orders.insert(bookId, num, address);
+
+            return RedirectToAction("orderList");
+        }
+
+        //订单列表
+        public ActionResult orderList()
+        {
+            List<db.sv_Orders> list = db.bll.orders.getOrders();
+            return View(list);
+        }
+    }
+}
